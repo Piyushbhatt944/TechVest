fc17e55cfded33dcac5070c941e62a2335f187b8
Piyushbhatt944 <piyushbhatt944@gmail.com>
2026-06-16 02:23:17 +0530

    chore: initialize React Vite project structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>TechVest Skeleton</h1>
+      <p>Project initialized.</p>
+    </div>
+  );
+}
+
+export default App;
